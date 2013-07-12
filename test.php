5dfd70e5a650f04807f37b4eab093d8472d0f183
Loganathane V <vlogus@gmail.com>
2013-07-12 17:41:32 +0530

    -

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once('gistfy_mysql.php');
+include_once('gistfy.php');
 include_once ('example\config.inc');
 
 $string = 'The fourth Wells account moving to another agency is the packaged paper-products division of Georgia-Pacific Corp., which arrived at Wells only last fall. Like Hertz and the History Channel, it is also leaving for an Omnicom-owned agency, the BBDO South unit of BBDO Worldwide. BBDO South in Atlanta, which handles corporate advertising for Georgia-Pacific, will assume additional duties for brands like Angel Soft toilet tissue and Sparkle paper towels, said Ken Haldin, a spokesman for Georgia-Pacific in Atlanta.';
